f17539f2e906b26d046203627b6ad84ccd196c7b
bhooomyy <bhattbhoomi0111@gmail.com>
2026-02-09 16:26:23 -0500

    Add query to count customer visit days
    Add SQL to answer question 2: count distinct order_date per customer (select customer_id, count(distinct order_date) from sales group by customer_id). Also minor whitespace additions.

--- Danny'sDinner.sql
+++ Danny'sDinner.sql
@@ -1,3 +1,7 @@
 -- 1. What is the total amount each customer spent at the restaurant?
 select s.customer_id,sum(m.price)from sales as s join menu as m on s.product_id=m.product_id group by s.customer_id order by customer_id asc;
 
+
+-- 2. How many days has each customer visited the restaurant?
+select customer_id,count(distinct order_date) from sales group by customer_id;
+
