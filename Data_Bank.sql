e21090f8c8edaa75bcf2cf8c6eb5d0cbf9f8eb6a
bhooomyy <bhattbhoomi0111@gmail.com>
2026-02-25 18:02:54 -0500

    Count customers per region
    -Joined customer_nodes with regions table.
    -Counted distinct customer_id for each region_name.

--- Data_Bank.sql
+++ Data_Bank.sql
@@ -9,3 +9,11 @@ r.region_name,
 count(distinct node_id) as cnt_node 
 from customer_nodes c join regions r on c.region_id=r.region_id
 group by r.region_name;
+
+-- 3. How many customers are allocated to each region?
+select 
+r.region_name,
+count(distinct customer_id) as cnt_node 
+from customer_nodes c join regions r on c.region_id=r.region_id
+group by r.region_name;
+
