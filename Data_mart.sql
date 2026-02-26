bc58cd704bedac45f3ad846a1b454c8718a73f5d
bhooomyy <bhattbhoomi0111@gmail.com>
2026-02-26 10:05:01 -0500

    Add week_number derived from day-of-year logic
    -Added week_number column to weekly_sales
    -Computed week buckets using CEIL(EXTRACT(DOY FROM week_date) / 7.0)
    -Ensures:
    Days 1–7 → Week 1
    Days 8–14 → Week 2
    Continues sequentially through the year
    -Uses day-of-year (DOY) instead of day-of-month to avoid monthly reset issue

--- Data_mart.sql
+++ Data_mart.sql
@@ -7,3 +7,10 @@ update weekly_sales set week_date=to_date(week_date, 'dd-mm-yy');
 alter table weekly_sales
 alter column week_date type date
 using to_date(week_date,'yyyy-mm-dd');
+
+-- Add a week_number as the second column for each week_date value, for example any value from the 1st of January to 7th of January will be 1, 8th to 14th will be 2 etc
+alter table weekly_sales
+add column week_number integer;
+-- update weekly_sales set week_number = ceil(extract(day from week_date) / 7.0);
+update weekly_sales
+set week_number=ceil(extract(doy from week_date) / 7.0);
